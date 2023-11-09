107f35cd0a2c6732590c08b7d89bd74b1ed4ab31
Exie <30753109+jxbc@users.noreply.github.com>
2023-11-09 05:27:43 +0200

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,7 +1,7 @@
 cd /home
 mkdir bridge
 cd bridge
-curl -O https://github.com/Yisus7u7/termux-ngrok/archive/refs/heads/main.zip
+wget https://github.com/Yisus7u7/termux-ngrok/archive/refs/heads/main.zip
 unzip main.zip
 rm main.zip
 cd termux-ngrok-main
