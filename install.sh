18585f94dff473d468a75628c50d45d47e5e2b77
Exie <30753109+jxbc@users.noreply.github.com>
2023-11-11 01:51:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ cd rc
 curl -O https://raw.githubusercontent.com/jxbc/termux-remote-control/main/rc.js
 npm i pm2 -g
 npm i ws
-node rc
+curl -O https://raw.githubusercontent.com/jxbc/termux-remote-control/main/bridge.sh
+bash bridge.sh
+clear
+echo "INSTALL COMPLETE!"
+node rc.js
