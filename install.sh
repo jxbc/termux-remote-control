81a16adff5408947066cbb8ceb2513bfd1beb58b
Exie <30753109+jxbc@users.noreply.github.com>
2023-12-05 18:21:45 +0200

    v1.6.9

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ curl -O https://raw.githubusercontent.com/jxbc/termux-remote-control/main/rc.js
 npm i pm2 -g
 npm i ws
 curl -O https://raw.githubusercontent.com/jxbc/termux-remote-control/main/bridge.sh
+apt install wget
 bash bridge.sh
 clear
 echo "INSTALL COMPLETE!"
